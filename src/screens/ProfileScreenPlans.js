bdc927cfe6c1920816ef8fe226b4507875057502
Zedan Saheer <zed.saheer5@gmail.com>
2021-08-14 08:49:13 +0530

    fixing some bugs and going to add extra functionality

--- src/screens/ProfileScreenPlans.js
+++ src/screens/ProfileScreenPlans.js
@@ -7,33 +7,50 @@ import { loadStripe } from '@stripe/stripe-js'
 
 const ProfileScreenPlans = () => {
 
-    const loadCheckout = async (priceId) =>{
+    const loadCheckout = async (priceId) => {
         const docRef = await db
-        .collection("customers")
-        .doc(user.uid)
-        .collection("checkout_sessions")
-        .add({
-            price : priceId,
-            success_url : window.location.origin,
-            cancel_url : window.location.origin,
-        });
+            .collection("customers")
+            .doc(user.uid)
+            .collection("checkout_sessions")
+            .add({
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin,
+            });
 
         docRef.onSnapshot(async (snap) => {
-            const {error , sessionId} = snap.data()
+            const { error, sessionId } = snap.data()
 
-            if(error){
+            if (error) {
                 alert(`something went wrong : ${error.message}`)
             }
 
-            if (sessionId){
+            if (sessionId) {
                 const stripe = await loadStripe("pk_test_51JNvIQSGM89JBLeEparqS1p4qvNP2GozSuH3Q969LZ912ucSY0IROjSBxoC69iQD56Q3pr3v2fDMvXSuLHPuYZgG00AXrZcAg4");
-                stripe.redirectToCheckout({sessionId});
+                stripe.redirectToCheckout({ sessionId });
             }
         });
     };
 
     const [products, setProducts] = useState([]);
     const user = useSelector(selectUser);
+    const [subscription, setSubscription] = useState(null);
+
+    useEffect(() => {
+        db.collection("customers")
+            .doc(user.uid)
+            .collection("subscriptions")
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(async (subscription) => {
+                    setSubscription({
+                        role: subscription.data().role,
+                        currentfPeriodStart: subscription.data().current_period_start,
+                        currentPeriodEnd: subscription.data().current_period_end,
+                    })
+                })
+            })
+    }, [user.uid])
 
     useEffect(() => {
         db.collection("products")
@@ -51,29 +68,60 @@ const ProfileScreenPlans = () => {
                         }
                     })
                 })
-             setProducts(products)
+                setProducts(products)
             })
-       
+
     }, [])
+
+    console.log(new Date(subscription?.current_period_end).toLocaleDateString());
 
     return (
         <>
-        <div className="profilescreen-box-content-plan-title">
-        <h3>Plans</h3></div>
-        <div className="profilescreen-box-content-plan-date">
-                <h4>Renewal Date :</h4>
+            <div className="profilescreen-box-content-plan-title">
+                <h3>Plans</h3>
             </div>
-        <div className="profilescreen-box-content-plans-container">
-        {Object.entries(products).map(([productId,productData])=>{
-              return (
-                <div className="profilescreen-box-content-plan" key={productId}>
-                    <div className="profilescreen-box-content-plan-detail"><span className="profilescreen-box-content-plan-detail-name">{productData.name}</span><span className="profilescreen-box-content-plan-detail-vq">{productData.description}</span></div>
-                    <div className="profilescreen-box-content-plan-button">
-                    <button className="profile-screen-subscribe" onClick={()=> loadCheckout(productData.prices.priceId)}>Subscribe</button></div>
-                </div>
-              )
-        })}
-        </div>
+
+            <div className="profilescreen-box-content-plan-date">
+
+                {subscription && (<h4> Renewal Date : {new Date(subscription?.current_period_end).toLocaleDateString()} </h4>)}
+
+            </div>
+
+            <div className="profilescreen-box-content-plans-container">
+                {Object.entries(products).map(([productId, productData]) => {
+
+                    const isCurrentPackage = productData.name?.toLowerCase().includes(subscription?.role);
+
+
+                    return (
+                        <div key={productId}>
+
+                            <div className="profilescreen-box-content-plan">
+
+                                {isCurrentPackage ?
+                                    (<>
+                                        <div className="profilescreen-box-content--current-plan-detail">
+                                            <span className="profilescreen-box-content-plan-detail-name">{productData.name}</span>
+                                            <span className="profilescreen-box-content-plan-detail-vq">{productData.description}</span>
+                                        </div>
+                                        <div className="profilescreen-box-content-current-plan-button">
+                                            <button className="profile-screen-current-subscribe" onClick={() => loadCheckout(productData.prices.priceId)}>Current Plan</button>
+                                        </div> </>)
+                                    :
+                                    (<>
+                                        <div className="profilescreen-box-content-plan-detail">
+                                            <span className="profilescreen-box-content-plan-detail-name">{productData.name}</span>
+                                            <span className="profilescreen-box-content-plan-detail-vq">{productData.description}</span>
+                                        </div>
+                                        <div className="profilescreen-box-content-plan-button">
+                                            <button className="profile-screen-subscribe" onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+                                        </div>
+                                    </>)}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
